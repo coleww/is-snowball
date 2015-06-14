b494834c882b08b41c3fec4510ee3b6a9f5c5445
Cole Willsea <coleww@gmail.com>
2015-06-14 15:45:46 -0700

    refactor to use every

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = function(text, reverse){
   var last, result = true
-  var words = text.split(" ").forEach(function(word){
+  return text.split(" ").every(function(word){
     var length = word.length
     var diff = reverse ? last - length : length - last
     if(last && diff !== 1) result = false
     last = length
+    return result
   })
-  return result
 }
